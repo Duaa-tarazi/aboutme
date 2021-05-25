9d08984a3b7f25e457df303a6ff62371bfba13c5
duaa-tarazi <dst08itm@yahoo.com>
2021-05-25 11:30:13 +0300

    edit aboutme repo

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # aboutme
  webpage that quizzes the user on fun facts gives the user some basic information about who iam and where i came from.
  
+ so i ask the user "Do i like movies?(yes,No)" and do i like cold or worm colors and tell them i like homedesiging so guses what the most color i like to use so if they know the right answer wich is yes i rais alert with yes i do like white and gray if the anwser is wrong i give him the write answer with adittional information and the fourth quastion is "do i like DIY and crafting(yes or no)"
+## now on lab 3 requrments i will add some quastion the first one it must have many attempt to answer so i will 
+ask them to guess how many sisters i have ? and 
--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
 'use strict';
-let username=prompt('Enter your name please:')
+let score = 0;
+/*let username=prompt('Enter your name please:')
 //num1
 let movie= prompt('Do i like movies?(yes,No)');
 if (movie ==='yes' || movie ==='y')
@@ -8,22 +9,35 @@ if (movie ==='yes' || movie ==='y')
 
       alert ('No i dont like watch movies ');
 }
+else{
+    alert('the right answer join the club');
+    score ++;
+}
 // num2
 let color=prompt ('Do i like worm colors or cold colors?' );
 if (color==='worm'|| color ==='worm colors')
-{   
+{
     // console.log(color);
 
     alert ('No i do like the cold color ');
 }
+else{
+    alert ('i like the cold color ');
+    score ++;
+}
 //num3
  let design=prompt('i like homedesiging and guses what the most color i like to use (white)yes or no? ');
  if (design==='no'|| design ==='n')
-{   
+{
     // console.log(design);
 
     alert ('Yes i like white and gray  ');
 }
+else {
+    alert ('i like white and gray  ');
+    score ++;
+}
+//num 4
 let diy=prompt('do i like DIY and crafting(yes or no)');
 diy.toUpperCase();
 
@@ -35,6 +49,46 @@ diy.toUpperCase();
 }
 else  {
     alert ('yes the right answer' );
+    score ++;
 }
 
-alert ('welcome'+' '+username+' '+'to my page ')
+alert ('welcome'+' '+username+' '+'to my page ')*/
+/*num5
+Add a question to the guessing game that takes in a numeric input by prompting the user to guess a number.
+*/
+let sister = prompt('guess how many sisters i have ?');
+sister = Number(sister);
+for (let i = 0; i <= 4; i++) { //It should give the user exactly four opportunities to get the correct answer
+    if (sister < 0) {
+        alert('enter alogic answer ');
+        sister = prompt('guess how many sisters i have ?');
+
+    }
+    else if (sister == 1) {
+        alert('you are right');
+        score++;
+        break;
+    }
+    else {
+        alert('No gusses again');
+        sister = prompt('guess how many sisters i have ?');
+
+    }
+
+}
+ alert('1 sister is the right answer');// tell the user the correct answer.
+//num6
+let fruitArr = ['banana', 'dates', 'orange', 'mango', 'peach'];// question that could have many possibilities (array i think) 
+    for (let index = 0; index === 6; index++) {  //Give the user 6 attempts to guess the correct answer.
+        let fruit = prompt('what is my faveret fruit?');
+        if (fruit !== fruitArr){
+        alert('not of my fav fruit');}
+        
+else {
+    alert('yes it is');
+    score++;
+}
+    }
+
+alert(score);
+
